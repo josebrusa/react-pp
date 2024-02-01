5e3b1338e1b2986fbaec2e6d90105e4ae02c36fe
josebrusa <josebrusa@outlook.com>
2024-02-01 18:08:37 +0100

    formulario Dinamico

--- src/03-forms/pages/DynamicForm.tsx
+++ src/03-forms/pages/DynamicForm.tsx
@@ -1,16 +1,40 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {Formik, Form} from 'formik'
+import * as Yup from 'yup'
 
 import formJson from '../data/custom-form.json'
 import { MySelect, MyTextInput } from '../components'
 
 const initialValues: {[key: string]: any} = {}
+const requiredFields: {[key: string]: any} = {}
 
 for(const input of formJson) {
-    initialValues[input.name] = input.value
+    initialValues[input.name] = input.value;
+
+    if(!input.validations ) continue;
+
+    let schema = Yup.string()
+
+    for (const rule of input.validations) {
+        if(rule.type === 'required') {
+            schema = schema.required('Este campo es requerido')
+        }
+        
+        if(rule.type === 'minLength'){
+            schema = schema.min((rule as any).value || 2, `Minimo de ${(rule as any).value || 2} carateres`)
+        }
+
+        if(rule.type === 'email'){
+            schema = schema.email(`Revise el formato del email`)
+        }
+
+
+
+        requiredFields[input.name] = schema;
+    }
 }
 
-
+const validationSchema = Yup.object({...requiredFields})
 
 export const DynamicForm = () => {
     return (
@@ -19,8 +43,9 @@ export const DynamicForm = () => {
 
             <Formik
                 initialValues={initialValues}
+                validationSchema={validationSchema}
                 onSubmit={(values) => {
-                    console.log(values)
+                console.log(values)
                 }}
             >
                 {
